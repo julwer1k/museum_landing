620388cff43aacd4f36256130e2f4cf5b6099026
ArtyWhit <julwer1k@gmail.com>
2024-05-19 23:13:14 +0300

    make build

---
+++ index.ed7d7bd1.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,autoplay:!0,pagination:{el:".swiper-pagination",clickable:!0},slidesPerView:1,spaceBetween:16,breakpoints:{768:{slidesPerView:2,spaceBetween:20},1280:{enabled:!1}}});
+//# sourceMappingURL=index.ed7d7bd1.js.map
